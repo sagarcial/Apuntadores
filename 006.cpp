04f8e9101f55603a020ce86ebd7f2df47a3bf7f5
Santiago Alexander Garcia Liscano <83739733+sagarcial@users.noreply.github.com>
2023-08-14 07:32:36 -0500

    Update 006.cpp

--- 006.cpp
+++ 006.cpp
@@ -1,37 +1,26 @@
-#include <iostream>
-#include <string>
-
+include <iostream>
 using namespace std;
 
 int main() {
-    const int numEstudiantes = 3;
-    const int numNotas = 3; 
-    string nombres[numEstudiantes];
-    int notas[numEstudiantes][numNotas];
+    struct Alumno {
+        string Nombre[3] = {"Andres", "Ivan", "Cristian"};
+        string *p_Nombre{Nombre};
+        int Notas[3] = {50, 45, 45}; 
+        int *p_Notas{Notas};
+    };
+    
+    Alumno alumno;
 
-    nombres[0] = "Juan";
-    notas[0][0] = 45;
-    notas[0][1] = 50;
-    notas[0][2] = 40;
+    cout << "Resultado de los apuntadores (Nombres):" << endl;
+    
+    for (int i = 0; i < 3; i++) {
+        cout << "Nombre: " << alumno.p_Nombre[i] << endl;
+    }
 
-    nombres[1] = "María";
-    notas[1][0] = 40;
-    notas[1][1] = 25;
-    notas[1][2] = 10;
+    cout << "Resultado de los apuntadores (Notas):" << endl;
 
-    nombres[2] = "Pedro";
-    notas[2][0] = 30;
-    notas[2][1] = 35;
-    notas[2][2] = 50;
-
-    for (int i = 0; i < numEstudiantes; i++) {
-        cout << "Datos del estudiante " << i + 1 << endl;
-        cout << "Nombre : " << nombres[i] << endl;
-        cout << "Notas del estudiante : ";
-        for (int j = 0; j < numNotas; j++) {
-            cout << notas[i][j] << "\t";
-        }
-        cout << endl;
+    for (int j = 0; j < 3; j++) {
+        cout << "Notas: " << alumno.p_Notas[j] << endl;
     }
 
     return 0;
